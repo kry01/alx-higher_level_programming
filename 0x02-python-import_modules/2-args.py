dcd0070c03be76fa8f9fadeef62003c2d7977d78
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-06-08 18:12:31 +0100

    add files for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
-import sys
-argv = sys.argv
-argc = len(argv)
-if argc == 2:
-    print("{} argument:".format(argc - 1))
-elif argc == 1:
-    print("{} arguments.".format(argc - 1))
-else:
-    print("{} arguments:".format(argc - 1))
-for i in range(1, argc):
-    print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv)
+    if argc == 2:
+        print("{} argument:".format(argc - 1))
+    elif argc == 1:
+        print("{} arguments.".format(argc - 1))
+    else:
+        print("{} arguments:".format(argc - 1))
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
