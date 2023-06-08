662de22e1cbc5e1a51008b4c9007034494bd65b9
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-06-08 18:11:12 +0100

    add files for task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ argv = sys.argv
 argc = len(argv)
 if argc == 2:
     print("{} argument:".format(argc - 1))
+elif argc == 1:
+    print("{} arguments.".format(argc - 1))
 else:
     print("{} arguments:".format(argc - 1))
 for i in range(1, argc):
