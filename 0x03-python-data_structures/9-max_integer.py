fa93128111171336695a23cd198059d74633b84e
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-06-13 00:18:18 +0100

    add files for ask 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
         return None
-    mx = 0
+    mx = my_list[0]
     for i in range(len(my_list)):
         if mx < my_list[i]:
             mx = my_list[i]
