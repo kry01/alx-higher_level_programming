60880dddb2b3d04d4d36eba665fc4c8ea98cd636
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-06-14 19:34:26 +0100

    fix some errors

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-    max_key = max(a_dictionary, key=lambda x: a_dictionary[x])
-    return max_key
+    return max(a_dictionary, key=a_dictionary.get)
