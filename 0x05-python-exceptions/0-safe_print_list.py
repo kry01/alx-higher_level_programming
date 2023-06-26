339fca78494767ecca5f358b03c08fbf1aa2b75e
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-06-26 20:38:16 +0100

    fix some errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,7 @@
 def safe_print_list(my_list=[], x=0):
+    if not my_list:
+        print("")
+        return (0)
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
