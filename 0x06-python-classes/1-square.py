ff2c1b61767d3a09390b558c30ec927f08ac6c92
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-08-08 13:48:05 +0100

    add file 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3a
+""" Suare class to represent a square """
+
+
+class Square():
+    """Empty square class """
+
+    def __init__(self, size):
+        self.__size = size
+    pass
