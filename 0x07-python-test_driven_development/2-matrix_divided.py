8a8c82b36993d4ee30ad8a68132ce8cca843eba2
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-10 17:06:53 +0100

    testing cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,7 +6,7 @@
 def matrix_divided(matrix, div):
     """ matrix_devided function thats devide the whole matrix by div number
     """
-    if (type(matrix) is not list and
+    if (type(matrix) is not list or
             not all((type(row) is list) for row in matrix)):
         raise TypeError("matrix must be a matrix "
                         "(list of lists) of integers/floats")
