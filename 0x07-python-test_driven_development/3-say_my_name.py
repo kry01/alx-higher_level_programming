51dad5a1b8d55bf5b8e93278ec0fd5e5a5c9c639
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-10 17:54:20 +0100

    say myname testing cases

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """
+say my name scarlet
 """
 
 
 def say_my_name(first_name, last_name=""):
+    """i am not in danger scarlet, i am the danger
+
+    Args:
+        first_name (_type_): first name to print
+        last_name (str, optional): last name to be printed "".
+
+    Raises:
+        TypeError: _description_
+        TypeError: _description_
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
