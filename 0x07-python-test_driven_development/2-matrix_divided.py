b3e5cf22f793e1e10268f109daa715dfd8122cb7
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-08-10 16:48:19 +0100

    add test cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,7 +9,7 @@ def matrix_divided(matrix, div):
         raise TypeError("matrix must be a matrix "
                         "(list of lists) of integers/floats")
 
-    elif all((len(row) == matrix[0]) for row in matrix):
+    elif not all((len(row) == len(matrix[0])) for row in matrix):
         raise TypeError("Each row of the matrix must"
                         " have the same size")
 
