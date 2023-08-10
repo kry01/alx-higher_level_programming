3df1fa52321691492c24b1c1e309a443734072e6
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-08-10 13:39:51 +0100

    add docstring

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" Contain the add_integer function
+"""
 
 def add_integer(a, b=98):
+    """ add_integer function to add two integers.
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
