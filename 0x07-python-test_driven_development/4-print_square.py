704f59678bc4fcd20fe72ea13d0cf5e2f69626e9
ELGHAZA-SAID <said.elghazal@edu.uiz.ac.ma>
2023-08-10 18:20:33 +0100

    testing cases

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" print square
+"""
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for i in range(size):
+            print('#'*size)
